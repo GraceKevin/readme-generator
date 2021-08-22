494ed19eb17d2f2bb5e61806f87d4345a508ef97
Kevin Grace <Kevin@kevins-air.home>
2021-08-22 12:32:46 -0400

    added licensing/badges to generateJS

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-// const (writeToFile) = require('./utils/generateMarkdown');
+const (writeToFile) = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const promptUser = () => {
@@ -26,7 +26,7 @@ const promptUser = () => {
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    console.log('Is this thing working?');
+
 };
 
 // TODO: Create a function to initialize app
