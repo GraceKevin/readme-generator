ac09de582ea89e0d839f520b8804e194b3f826fa
Kevin Grace <Kevin@kevins-air.home>
2021-08-22 15:09:01 -0400

    questions now generate readme

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-# testreadme
+# undefined
   undefined
-  undefined
+  * [Information](#information)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](test)
+  * [Questions](#questions)
+  * [License](#license)
+  
+  # Information
+    undefined
+  # Installation
+    npm install
+  # Usage
+    This may be used in any project in order to help generate a readme.md
+  # Contributing
+    Any future updates
+  # Test
+    npm run test
+  # Questions
+    undefined
+  
+  * You can see more of my work by searching my username: undefined
+  * Or by accessing my Github here. (https://github.com/undefined)
+  *  You may also reach out to me directly at undefined
+  # License
+
+   
   undefined
 
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const util = require('util');
 const generate = require('./utils/generateMarkdown');
 
 // Create an array of questions for user input
@@ -9,28 +8,68 @@ const questions = [
     {
         type: 'input',
         name: 'Github',
-        message: 'What is your Github Username?',
+        message: 'What is your Github Username? (Required)',
+        validate: (Username) => {
+            if (Username) {
+                return true;
+            } 
+            else {
+                console.log("Please enter your Github Username.");
+            }
+        }
     },
     {
         type: 'input',
         name: 'Title',
-        message: 'What is the name of the project?',
+        message: 'What is the name of the project? (Required)',
+        validate: (Title) => {
+            if (Title) {
+                return true;
+            }
+            else {
+                console.log("Please enter the title of your project.");
+            }
+        }
     },
     {
         type: 'input',
         name: 'Description',
-        message: 'How would you describe the project?',
+        message: 'How would you describe the project? (Required)',
+        validate: (Description) => {
+            if (Description) {
+                return true
+            }
+            else {
+                console.log("Please enter a description for the project.");
+            }
+        }
     },
     {
         type: "input",
-        name: "Email",
-        message: "Please provide your email address:"
+        name: "yourEmail",
+        message: "Please provide your email address: (Required)",
+        validate: (yourEmail) => {
+            if (yourEmail) {
+                return true
+            }
+            else {
+                console.log("Please enter your email address.");
+            }
+        }
     },
     {
         type: "list",
         name: "License",
-        message: "Which license will be included in the project?",
-        choices: ["MIT", "Apache", "GNP", "None"]
+        message: "Which license will be included in the project? (Required)",
+        choices: ["MIT", "Apache", "GNP", "None"],
+        validate: (License) => {
+            if (License) {
+                return true
+            }
+            else {
+                console.log("Please select an option for the license. Select none if none apply");
+            }
+        }
     },
     {
         type: "input",
@@ -47,19 +86,30 @@ const questions = [
     {
         type: "input",
         name: "Usage",
-        message: "What does any users need to know regarding the usage of this repository?"
+        message: "What does any users need to know regarding the usage of this repository? (Required)",
+        validate: (Usage) => {
+            if (Usage) {
+                return true
+            }
+            else {
+                console.log("Please enter how this project can be utilized.");
+            }
+        }
     },
     {
         type: "input",
         name: "Contributing",
-        message: "What does any users need to know regarding contibuting to this repository?"
+        message: "What does any users need to know regarding contibuting to this repository? (Required)",
+        validate: (Contributing) => {
+            if (Contributing)  {
+                return true
+            } 
+            else {
+                console.log("Please indicate to users how they may be able to contribute.");
+            }
+        }
     },
 ];
-
-const promptUser = () => {
-    return inquirer
-    .prompt(questions);
-}
 
 // Create a function to write README file
 function writeToFile(fileName, data) {
@@ -74,8 +124,9 @@ function writeToFile(fileName, data) {
 // Create a function to initialize app
 function init() {
     // console.log(promptUser);
-    inquirer.prompt(promptUser)
+    inquirer.prompt(questions)
     .then(function (userInput) {
+        console.log(userInput)
         writeToFile("Readme.md", generate(userInput))
     })
 };
